2774bd97be417c466a64d7ebc92a29d5817c8f55
JEALG <jalain.gosse@icloud.com>
2023-10-21 09:39:17 +0200

    Amélioration affichage

--- desktop/js/slack.js
+++ desktop/js/slack.js
@@ -14,7 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
-
+document.querySelector('div.callback a.decrypt').addEventListener('click', function() {
+    this.parentNode.querySelectorAll('span').forEach(function(e) {
+      e.classList.toggle('encrypt')
+    })
+    this.querySelector('i').classList.toggle('fa-eye-slash')
+  })
  function addCmdToTable(_cmd) {
     if (!isset(_cmd)) {
         var _cmd = {configuration: {}};
@@ -23,18 +28,14 @@
         _cmd.configuration = {};
     }
     var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
-    tr += '<td>';
-    tr += '<span class="cmdAttr" data-l1key="id"></span>';
-    tr += '</td>';
-    tr += '<td>';
-    tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">';
-    if (isset(_cmd.type) &&  _cmd.type == 'info') {
-      tr += '<input class="cmdAttr" data-l1key="type" value="info" style="display:none;" />';
-    }else{
-      tr += '<input class="cmdAttr" data-l1key="type" value="action" style="display:none;" />';
-    }
-     tr += '<input class="cmdAttr" data-l1key="subtype" value="message" style="display:none;" />';
-     tr += '</td>';
+    tr += '<td class="hidden-xs">'
+    tr += '<span class="cmdAttr" data-l1key="id"></span>'
+     tr += '<td>'
+     tr += '<div class="input-group">'
+     tr += '<input class="cmdAttr form-control input-sm roundedLeft" data-l1key="name" placeholder="{{Nom de la commande}}">'
+     tr += '<span class="input-group-btn"><a class="cmdAction btn btn-sm btn-default" data-l1key="chooseIcon" title="{{Choisir une icône}}"><i class="fas fa-icons"></i></a></span>'
+     tr += '<span class="cmdAttr input-group-addon roundedRight" data-l1key="display" data-l2key="icon" style="font-size:19px;padding:0 5px 0 0!important;"></span>'
+     tr += '</div>'
      tr += '<td>';
      if (!isset(_cmd.type) || _cmd.type == 'action') {
          tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="webhook" placeholder="{{Webhook}}"></td>';
@@ -46,10 +47,10 @@
      }
      tr += '</td>';
      tr += '<td>';
-     if (is_numeric(_cmd.id)) {
-         tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
-         tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
-     }
+        if (is_numeric(_cmd.id)) {
+            tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure" title="{{Configuration avancée}}"><i class="fas fa-cogs"></i></a> ';
+            tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
+        }
      tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i>';
      tr += '</td>';
      tr += '</tr>';
